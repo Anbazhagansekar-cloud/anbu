7198620a866d1f66386ae6bc9902714fbffc183d
Anbazhagansekar-cloud <160880709+Anbazhagansekar-cloud@users.noreply.github.com>
2024-02-26 23:11:26 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+sudo apt update -y
+wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+sudo apt update -y
+sudo apt install temurin-17-jdk -y
+/usr/bin/java --version
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+##Install Docker and Run SonarQube as Container
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo usermod -aG docker ubuntu
+sudo usermod -aG docker jenkins  
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+
+#install trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy -y
+
+#install maven
+sudo apt-get update
+sudo apt install openjdk-17-jdk -y
+sudo apt install maven -y
+
+#Configure JFrog Artifactory
+sudo mkdir -p $JFROG_HOME/artifactory/var/etc/
+cd $JFROG_HOME/artifactory/var/etc/
+sudo touch ./system.yaml
+sudo chown -R 1030:1030 $JFROG_HOME/artifactory/var
+docker run --name artifactory -v $JFROG_HOME/artifactory/var/:/var/opt/jfrog/artifactory -d -p 8081:8081 -p 8082:8082 releases-docker.jfrog.io/jfrog/artifactory-oss:latest
+
+
+#Create AWS EKS Cluster and Download the Config/Secret file for EKS Cluster
+#Install kubectl on Jenkins
+ sudo apt update
+ sudo apt install curl
+ curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+ kubectl version --client
+
+
+#Install AWS Cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+
+#Installing  eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+cd /tmp
+sudo mv /tmp/eksctl /bin
+eksctl version
+
+#Create an IAM Role and attache it to EC2 instance
+#aws iam create-role --role-name MyEC2Role --assume-role-policy-document '{"Version": "2012-10-17","Statement": [{"Effect": "Allow","Principal": {"Service": "ec2.amazonaws.com"},"Action": "sts:AssumeRole"}]}'
+#aws iam attach-role-policy --role-name MyEC2Role --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess
+#aws iam attach-role-policy --role-name MyEC2Role --policy-arn arn:aws:iam::aws:policy/AWSCloudFormationFullAccess
+#aws iam attach-role-policy --role-name MyEC2Role --policy-arn arn:aws:iam::aws:policy/IAMFullAccess
+#aws iam attach-role-policy --role-name MyEC2Role --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
+#INSTANCE_ID=$(curl -s http://172.31.33.40/latest/meta-data/instance-id)
+#aws ec2 associate-iam-instance-profile --instance-id $INSTANCE_ID --iam-instance-profile Name=MyEC2Role
+
+
+#Setup Kubernetes using eksctl
+#Refer--https://github.com/aws-samples/eks-workshop/issues/734
+eksctl create cluster --name virtualtechbox-cluster \
+--region ap-south-1 \
+--node-type t2.small \
+--nodes 3 \
+
+git config --global user.name "Anbazhagansekar-cloud"
+git config --global user.email "anbazhagansekar1996cloud@gmail.com"
+git clone https://github.com/Anbazhagansekar-cloud/anbu.git
